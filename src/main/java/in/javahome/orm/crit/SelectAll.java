28e3ec82eab77da7bb5505524393d48a1da151d3
javahome <hari.kammana@gmail.com>
2015-12-18 19:42:12 +0530

    Composit primary key

--- src/main/java/in/javahome/orm/crit/SelectAll.java
+++ src/main/java/in/javahome/orm/crit/SelectAll.java
@@ -6,6 +6,7 @@ import in.javahome.orm.entity.Employee;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.LockMode;
 import org.hibernate.Session;
 
 
@@ -15,6 +16,10 @@ public static void main(String[] args) {
 	Session session = HibUtil.getSession();
 	
 	Criteria criteria = session.createCriteria(Employee.class);
+	
+	criteria.setLockMode(LockMode.PESSIMISTIC_READ);
+	criteria.setFirstResult(0);
+	criteria.setMaxResults(40);
 	List<Employee> employees = criteria.list();
 	
 	for (Employee emp : employees) {
